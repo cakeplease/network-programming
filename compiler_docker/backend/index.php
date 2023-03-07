04d23a5e7811572cc7c935422dce1c1cc3a96366
Kasia <kasia.szlejter@gmail.com>
2023-03-07 12:23:19 +0100

    Finished compiler

--- compiler_docker/backend/index.php
+++ compiler_docker/backend/index.php
@@ -1,8 +1,17 @@
 <?php
-//var_dump($_POST["code"]);
 file_put_contents("compile.php", $_POST["code"]);
+$output = null;
+$res_code = null;
+$run_file = exec("php -f compile.php", $output, $res_code);
 
-$output = "";
-$run_file = exec("php -f compile.php", $output);
+if ($res_code === 0) {
+    $res = [];
+    $res['status'] = 'success';
+    $res['result'] = $output;
+} else {
+    $res = [];
+    $res['status'] = 'failed';
+    $res['result'] = $output;
+}
 
-echo json_encode($output);
+echo json_encode($res);
